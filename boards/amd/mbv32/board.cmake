5e2826b044b0912ee9ea3b19996bfab55390d8e8
Michal Simek <michal.simek@amd.com>
2025-02-14 16:44:59 +0100

    mbv32: Switch to Xilinx qemu from upstream one
    Zephyr SDK 0.17.1 has been updated to use Xilinx v2025.1 QEMU version also
    with enabling MB-V. Switch to this version which provides more models
    compare to upstream QEMU.
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    State: pending

--- boards/amd/mbv32/board.cmake
+++ boards/amd/mbv32/board.cmake
@@ -1,18 +1,19 @@
 #
-# Copyright (c) 2024 Advanced Micro Devices, Inc.
+# Copyright (c) 2024 - 2025 Advanced Micro Devices, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 include(${ZEPHYR_BASE}/boards/common/xsdb.board.cmake)
 set(SUPPORTED_EMU_PLATFORMS qemu)
-set(QEMU_ARCH riscv32)
+set(QEMU_ARCH xilinx-riscv32)
 set(QEMU_CPU_TYPE_${ARCH} riscv32)
 
 set(QEMU_FLAGS_${ARCH}
-	-machine amd-microblaze-v-virt
+	-machine riscv-fdt
+	-hw-dtb ${PROJECT_BINARY_DIR}/${BOARD}-qemu.dtb
 	-nographic
-	-net nic,netdev=eth0 -netdev user,id=eth0 -net nic,netdev=eth1 -netdev user,id=eth1
+	-net nic,netdev=eth0 -netdev user,id=eth0
 	-m 2g
 )
 
