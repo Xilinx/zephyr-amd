Xilinx/zephyr-amd

commit 9c5128af3471a6c72e5cca27549af9eb396fa315

drivers: flash: spi_nor: Implement configurable bus width for enhanced SPI modes
Enable configurable transmit and receive bus widths to support dual and quad
SPI operations. The driver now extracts spi_tx_bus_width and spi_rx_bus_width
from devicetree properties to select appropriate SPI commands and configure
bus settings accordingly.

Signed-off-by: Srikanth Boyapally <srikanth.boyapally@amd.com>
Message-ID: <20250825091926.44527-2-srikanth.boyapally@amd.com>

drivers/flash/spi_nor.c
@@ -159,6 +159,10 @@ struct spi_nor_config {
 	bool requires_ulbpr_exist:1;
 	bool wp_gpios_exist:1;
 	bool hold_gpios_exist:1;
+
+	/* SPI bus width for TX and RX operations */
+	uint8_t spi_tx_bus_width;
+	uint8_t spi_rx_bus_width;
 };
 
 /**
@@ -375,18 +379,43 @@ static int spi_nor_access(const struct device *const dev,
 	bool is_addressed = (access & NOR_ACCESS_ADDRESSED) != 0U;
 	bool is_write = (access & NOR_ACCESS_WRITE) != 0U;
 	uint8_t buf[5] = { 0 };
-	struct spi_buf spi_buf[2] = {
-		{
-			.buf = buf,
-			.len = 1,
-		},
-		{
-			.buf = data,
-			.len = length
-		}
+
+	struct spi_buf tx_spi_buf[2];
+	struct spi_buf rx_spi_buf[3];
+	struct spi_buf_set tx_set = {
+		.buffers = tx_spi_buf,
+		.count = (length != 0) ? 2 : 1,
+	};
+	struct spi_buf_set rx_set = {
+		.buffers = rx_spi_buf,
+		.count = 2,
 	};
 
 	buf[0] = opcode;
+	tx_spi_buf[0] = (struct spi_buf){
+		.buf = buf,
+		.len = 1,
+		.bus_width = BUS_WIDTH_SPI,
+	};
+
+	tx_spi_buf[1] = (struct spi_buf){
+		.buf = is_write ? data : NULL,
+		.len = is_write ? length : 0,
+		.bus_width = BUS_WIDTH_SPI,
+	};
+
+	rx_spi_buf[0] = (struct spi_buf){
+		.buf = NULL,
+		.len = 0,
+		.bus_width = BUS_WIDTH_SPI,
+	};
+
+	rx_spi_buf[1] = (struct spi_buf){
+		.buf = data,
+		.len = length,
+		.bus_width = BUS_WIDTH_SPI,
+	};
+
 	if (is_addressed) {
 		bool access_24bit = (access & NOR_ACCESS_24BIT_ADDR) != 0;
 		bool access_32bit = (access & NOR_ACCESS_32BIT_ADDR) != 0;
@@ -402,22 +431,35 @@ static int spi_nor_access(const struct device *const dev,
 
 		if (use_32bit) {
 			memcpy(&buf[1], &addr32.u8[0], 4);
-			spi_buf[0].len += 4;
+			tx_spi_buf[0].len += 4;
 		} else {
 			memcpy(&buf[1], &addr32.u8[1], 3);
-			spi_buf[0].len += 3;
+			tx_spi_buf[0].len += 3;
 		}
-	};
+	}
 
-	const struct spi_buf_set tx_set = {
-		.buffers = spi_buf,
-		.count = (length != 0) ? 2 : 1,
-	};
-
-	const struct spi_buf_set rx_set = {
-		.buffers = spi_buf,
-		.count = 2,
-	};
+	if (is_addressed && length) {
+		if ((driver_cfg->spi_tx_bus_width & SPI_MODE_MASK) && is_write) {
+			tx_spi_buf[1] = (struct spi_buf){
+				.buf = data,
+				.len = length,
+				.bus_width = driver_cfg->spi_tx_bus_width,
+			};
+		}
+		if (driver_cfg->spi_rx_bus_width & SPI_MODE_MASK) {
+			rx_spi_buf[1] = (struct spi_buf){
+				.buf = NULL,
+				.len = 8,
+				.bus_width = driver_cfg->spi_rx_bus_width,
+			};
+			rx_spi_buf[2] = (struct spi_buf){
+				.buf = data,
+				.len = length,
+				.bus_width = driver_cfg->spi_rx_bus_width,
+			};
+			rx_set.count += 1;
+		}
+	}
 
 	if (is_write) {
 		return spi_write_dt(&driver_cfg->spi, &tx_set);
@@ -775,6 +817,7 @@ static int mxicy_configure(const struct device *dev, const uint8_t *jedec_id)
 static int spi_nor_read(const struct device *dev, off_t addr, void *dest,
 			size_t size)
 {
+	const struct spi_nor_config *const driver_cfg = dev->config;
 	const size_t flash_size = dev_flash_size(dev);
 	int ret;
 
@@ -785,7 +828,11 @@ static int spi_nor_read(const struct device *dev, off_t addr, void *dest,
 
 	acquire_device(dev);
 
-	ret = spi_nor_cmd_addr_read(dev, SPI_NOR_CMD_READ, addr, dest, size);
+	if (driver_cfg->spi_rx_bus_width == BUS_WIDTH_QUAD) {
+		ret = spi_nor_cmd_addr_read(dev, SPI_NOR_CMD_QREAD, addr, dest, size);
+	} else {
+		ret = spi_nor_cmd_addr_read(dev, SPI_NOR_CMD_READ, addr, dest, size);
+	}
 
 	release_device(dev);
 	return ret;
@@ -823,6 +870,7 @@ static int spi_nor_write(const struct device *dev, off_t addr,
 			 const void *src,
 			 size_t size)
 {
+	const struct spi_nor_config *const driver_cfg = dev->config;
 	const size_t flash_size = dev_flash_size(dev);
 	const uint16_t page_size = dev_page_size(dev);
 	int ret;
@@ -854,9 +902,13 @@ static int spi_nor_write(const struct device *dev, off_t addr,
 			if (ret != 0) {
 				break;
 			}
-
-			ret = spi_nor_cmd_addr_write(dev, SPI_NOR_CMD_PP, addr,
+			if (driver_cfg->spi_tx_bus_width == BUS_WIDTH_QUAD) {
+				ret = spi_nor_cmd_addr_write(dev, SPI_NOR_CMD_PP_1_1_4, addr,
 						src, to_write);
+			} else {
+				ret = spi_nor_cmd_addr_write(dev, SPI_NOR_CMD_PP, addr,
+						src, to_write);
+			}
 			if (ret != 0) {
 				break;
 			}
@@ -1673,6 +1725,8 @@ static const struct flash_driver_api spi_nor_api = {
 	.requires_ulbpr_exist = DT_INST_PROP(idx, requires_ulbpr),				\
 	.wp_gpios_exist = DT_INST_NODE_HAS_PROP(idx, wp_gpios),					\
 	.hold_gpios_exist = DT_INST_NODE_HAS_PROP(idx, hold_gpios),				\
+	.spi_tx_bus_width = DT_INST_PROP_OR(idx, spi_tx_bus_width, 1),				\
+	.spi_rx_bus_width = DT_INST_PROP_OR(idx, spi_rx_bus_width, 1),				\
 	IF_ENABLED(INST_HAS_LOCK(idx), (.has_lock = DT_INST_PROP(idx, has_lock),))		\
 	IF_ENABLED(CONFIG_SPI_NOR_SFDP_MINIMAL, (CONFIGURE_4BYTE_ADDR(idx)))			\
 	IF_ENABLED(CONFIG_SPI_NOR_SFDP_DEVICETREE,						\

drivers/flash/spi_nor.h
@@ -115,4 +115,16 @@
 
 #define CMD_RDCR 0x15 /* Read the configuration register. */
 
+/**
+ * @name Bus Width
+ * @{
+ *
+ * Bus width to the SPI bus used for read/write transfers.
+ */
+#define BUS_WIDTH_SPI     1     /**< Single line */
+#define BUS_WIDTH_DUAL    2     /**< Dual lines */
+#define BUS_WIDTH_QUAD    4     /**< Quad lines */
+#define BUS_WIDTH_OCTAL   8     /**< Octal lines */
+/** @} */
+
 #endif /*__SPI_NOR_H__*/

dts/bindings/mtd/jedec,spi-nor-common.yaml
@@ -102,3 +102,15 @@ properties:
       low power mode.
 
       Only supported on Macronix MX25R Ultra Low Power series.
+
+  spi-rx-bus-width:
+    type: int
+    description:
+      Bus width to the SPI bus used for read transfers.
+    enum: [1, 2, 4, 8]
+
+  spi-tx-bus-width:
+    type: int
+    description:
+      Bus width to the SPI bus used for write transfers.
+    enum: [1, 2, 4, 8]

include/zephyr/drivers/spi.h
@@ -429,6 +429,8 @@ struct spi_buf {
 	 * buffer) or the length of bytes that should be skipped (as RX buffer).
 	 */
 	size_t len;
+	/** Bus width for write and read transfers */
+	size_t bus_width;
 };
 
 /**

commit 819b73470190fbed5af4c0d15e44c8a7dbee5a1c

drivers: flash: Fix compatibility with standard SPI drivers
Fix SPI NOR flash driver to work correctly with standard SPI drivers
that don't support separate TX and RX buffers for transfers.

Fixes: 9c5128af347 ("drivers: flash: spi_nor: Implement configurable bus width for enhanced SPI modes")
Signed-off-by: Srikanth Boyapally <srikanth.boyapally@amd.com>
Message-ID: <20250904063527.48988-1-srikanth.boyapally@amd.com>

drivers/flash/spi_nor.c
@@ -378,6 +378,8 @@ static int spi_nor_access(const struct device *const dev,
 	struct spi_nor_data *const driver_data = dev->data;
 	bool is_addressed = (access & NOR_ACCESS_ADDRESSED) != 0U;
 	bool is_write = (access & NOR_ACCESS_WRITE) != 0U;
+	bool is_extended_spi = (driver_cfg->spi_tx_bus_width & SPI_MODE_MASK) ||
+		(driver_cfg->spi_rx_bus_width & SPI_MODE_MASK);
 	uint8_t buf[5] = { 0 };
 
 	struct spi_buf tx_spi_buf[2];
@@ -399,8 +401,8 @@ static int spi_nor_access(const struct device *const dev,
 	};
 
 	tx_spi_buf[1] = (struct spi_buf){
-		.buf = is_write ? data : NULL,
-		.len = is_write ? length : 0,
+		.buf = is_extended_spi ? (is_write ? data : NULL) : data,
+		.len = is_extended_spi ? (is_write ? length : 0) : length,
 		.bus_width = BUS_WIDTH_SPI,
 	};
 
@@ -465,7 +467,10 @@ static int spi_nor_access(const struct device *const dev,
 		return spi_write_dt(&driver_cfg->spi, &tx_set);
 	}
 
-	return spi_transceive_dt(&driver_cfg->spi, &tx_set, &rx_set);
+	if (is_extended_spi)
+		return spi_transceive_dt(&driver_cfg->spi, &tx_set, &rx_set);
+
+	return spi_transceive_dt(&driver_cfg->spi, &tx_set, &tx_set);
 }
 
 #define spi_nor_cmd_read(dev, opcode, dest, length) \