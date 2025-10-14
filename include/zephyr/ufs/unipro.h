d3f1e94c74a65179fcf02e80773043c6ed1e620e
Ajay Neeli <ajay.neeli@amd.com>
2025-10-14 17:05:10 +0530

    ufs: unipro: add power mode attributes and enums
    Add power mode attributes and enumerations for UFS UniPro layer.
    This includes power mode states, gear rates, lane configurations that
    are needed for UFS power mode management.
    
    Signed-off-by: Ajay Neeli <ajay.neeli@amd.com>
    Message-ID: <20251014113515.22365-2-ajay.neeli@amd.com>
    State: waiting
    Link: https://github.com/zephyrproject-rtos/zephyr/pull/93528

--- include/zephyr/ufs/unipro.h
+++ include/zephyr/ufs/unipro.h
@@ -18,7 +18,14 @@
 #define PA_CONNECTEDRXDATALANES	0x1581
 #define PA_RXGEAR		0x1583
 #define PA_RXTERMINATION	0x1584
+#define PA_MAXRXPWMGEAR		0x1586
+#define PA_MAXRXHSGEAR		0x1587
 #define PA_TXHSADAPTTYPE	0x15D4
+
+/* Adapt type for PA_TXHSADAPTTYPE attribute */
+#define PA_REFRESH_ADAPT       0x00
+#define PA_INITIAL_ADAPT       0x01
+#define PA_NO_ADAPT            0x03
 
 /* Transport Layer Attributes */
 #define T_CONNECTIONSTATE	0x4020
@@ -26,5 +33,46 @@
 /* Other Attributes */
 #define VS_MPHYCFGUPDT		0xD085
 #define VS_MPHYDISABLE		0xD0C1
+
+/* PA power modes */
+enum ufs_pa_pwr_mode {
+	FAST_MODE	= 1,
+	SLOW_MODE	= 2,
+	FASTAUTO_MODE	= 4,
+	SLOWAUTO_MODE	= 5,
+	UNCHANGED	= 7,
+};
+
+/* PA TX/RX Frequency Series */
+enum ufs_hs_gear_rate {
+	PA_HS_MODE_A	= 1,
+	PA_HS_MODE_B	= 2,
+};
+
+enum ufs_pwm_gear_tag {
+	UFS_PWM_DONT_CHANGE,	/* Don't change Gear */
+	UFS_PWM_G1,		/* PWM Gear 1 (default for reset) */
+	UFS_PWM_G2,		/* PWM Gear 2 */
+	UFS_PWM_G3,		/* PWM Gear 3 */
+	UFS_PWM_G4,		/* PWM Gear 4 */
+	UFS_PWM_G5,		/* PWM Gear 5 */
+	UFS_PWM_G6,		/* PWM Gear 6 */
+	UFS_PWM_G7,		/* PWM Gear 7 */
+};
+
+enum ufs_hs_gear_tag {
+	UFS_HS_DONT_CHANGE,	/* Don't change Gear */
+	UFS_HS_G1,		/* HS Gear 1 (default for reset) */
+	UFS_HS_G2,		/* HS Gear 2 */
+	UFS_HS_G3,		/* HS Gear 3 */
+	UFS_HS_G4,		/* HS Gear 4 */
+	UFS_HS_G5		/* HS Gear 5 */
+};
+
+enum ufs_lanes {
+	UFS_LANE_DONT_CHANGE,	/* Don't change Lane */
+	UFS_LANE_1,		/* Lane 1 (default for reset) */
+	UFS_LANE_2,		/* Lane 2 */
+};
 
 #endif /* ZEPHYR_INCLUDE_UFS_UNIPRO_H_ */
