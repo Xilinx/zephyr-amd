4f407e3555acfdcf592816d4cdfdc7989290ffb0
Harini T <harini.t@amd.com>
2025-12-05 21:16:26 +0530

    tests: watchdog: wdt_basic_api: Reorder Xilinx WWDT configuration
    Move xlnx,versal-wwdt timeout configuration as the DT has watchdog0
    alias.
    
    Signed-off-by: Harini T <harini.t@amd.com>
    Message-ID: <20251205154627.970616-5-harini.t@amd.com>
    [v4.3.0 update
    Origin: 666d9183ea73 ("tests: watchdog: wdt_basic_api: Reorder Xilinx WWDT configuration")
    ]
    State: pending

--- tests/drivers/watchdog/wdt_basic_api/src/test_wdt.c
+++ tests/drivers/watchdog/wdt_basic_api/src/test_wdt.c
@@ -107,10 +107,6 @@
 #define WDT_NODE DT_INST(0, andestech_atcwdt200)
 #define TIMEOUTS 0
 #define WDT_TEST_MAX_WINDOW 200U
-#elif DT_HAS_COMPAT_STATUS_OKAY(xlnx_versal_wwdt)
-#define WDT_NODE DT_COMPAT_GET_ANY_STATUS_OKAY(xlnx_versal_wwdt)
-#define WDT_TEST_MAX_WINDOW 20000U
-#define TIMEOUTS 0
 #endif
 #if DT_HAS_COMPAT_STATUS_OKAY(raspberrypi_pico_watchdog)
 #define WDT_TEST_MAX_WINDOW 8000U
@@ -122,6 +118,10 @@
 #endif
 #if DT_HAS_COMPAT_STATUS_OKAY(nxp_wdog32)
 #define WDT_TEST_MAX_WINDOW 1000U
+#endif
+#if DT_HAS_COMPAT_STATUS_OKAY(xlnx_versal_wwdt)
+#define WDT_TEST_MAX_WINDOW 20000U
+#define TIMEOUTS 0
 #endif
 
 #define WDT_TEST_STATE_IDLE        0
