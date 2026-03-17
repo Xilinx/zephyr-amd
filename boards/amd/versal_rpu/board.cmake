1ae58edc227470deb729e9c7bcd3a118430394e3
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2026-03-17 14:13:17 +0530

    boards: amd: versal_rpu: add QEMU simulation support
    Add QEMU simulation support for the AMD Versal RPU board:
     - versal_rpu-qemu.dts: QEMU-specific device tree
     - board.cmake: QEMU runner configuration
     - CMakeLists.txt: board build setup
     - versal_rpu.yaml: add qemu simulation target
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    Message-ID: <20260317084317.3374215-1-appana.durga.kedareswara.rao@amd.com>
    State: not-upstreamable

--- boards/amd/versal_rpu/board.cmake
+++ boards/amd/versal_rpu/board.cmake
@@ -3,3 +3,20 @@
 # SPDX-License-Identifier: Apache-2.0
 
 include(${ZEPHYR_BASE}/boards/common/xsdb.board.cmake)
+set(SUPPORTED_EMU_PLATFORMS qemu)
+set(QEMU_ARCH xilinx-aarch64)
+set(QEMU_CPU_TYPE_${ARCH} cortex-r5f)
+
+set(QEMU_FLAGS_${ARCH}
+  -machine arm-generic-fdt
+  -hw-dtb ${PROJECT_BINARY_DIR}/${BOARD}-qemu.dtb
+  -device loader,addr=0xff5e0300,data=0x16,data-len=4
+  -device loader,addr=0xff9a0100,data=0x1,data-len=4
+  -device loader,addr=0xff9a0000,data=0x48,data-len=4
+  -nographic
+  -m 2g
+)
+
+set(QEMU_KERNEL_OPTION
+  -device loader,cpu-num=2,file=\$<TARGET_FILE:\${logical_target_for_zephyr_elf}>
+)
